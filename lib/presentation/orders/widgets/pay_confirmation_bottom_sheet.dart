4015c691f08909cf7bc8bcd43a24d2f9b1880052
Евгений Сивицкий <>
2021-04-25 00:52:40 +0300

    added pay flow and description in order detailed

---
+++ lib/presentation/orders/widgets/pay_confirmation_bottom_sheet.dart
@@ -0,0 +1,78 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/presentation/components/widgets/buttons.dart';
+import 'package:flutter_app/presentation/orders/widgets/pay_success_bottom_sheet.dart';
+import 'package:flutter_app/utils/nx_colors.dart';
+import 'package:flutter_app/utils/styles.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+class PayConfirmationBottomSheet extends StatelessWidget {
+  const PayConfirmationBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 44, sigmaY: 44),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(.59),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 32),
+                Center(
+                  child: Text(
+                    'Внимание',
+                    style: primaryText16,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Center(
+                  child: Text(
+                    'Вы уверены, что хотите совершить оплату?',
+                    style: primaryText13,
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ActionButton(
+                  text: 'Да, оплатить',
+                  color: NXColors.systemGrey6,
+                  textStyle: primaryText16.copyWith(
+                    color: NXColors.orange,
+                  ),
+                  onPressed: () {
+                    showMaterialModalBottomSheet(
+                        animationCurve: Curves.easeInOut,
+                        context: context,
+                        backgroundColor: Colors.black.withOpacity(0.03),
+                        expand: true,
+                        builder: (context) =>
+                            PaySuccessBottomSheet()).then(
+                        (value) => Navigator.of(context).pop(true)); // is paid
+                  },
+                ),
+                const SizedBox(height: 16),
+                ActionButton(
+                  text: 'Отмена',
+                  color: NXColors.systemGrey6,
+                  textStyle: primaryText16.copyWith(
+                    color: NXColors.textSecondary,
+                  ),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
