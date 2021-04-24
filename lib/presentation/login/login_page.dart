074eb9e882eb018db65781c9bb4ef706be0daef6
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2021-04-24 12:56:24 +0300

    finish with login screen

--- lib/presentation/login/login_page.dart
+++ lib/presentation/login/login_page.dart
@@ -1,80 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/presentation/components/widgets/background.dart';
+import 'package:flutter_app/presentation/components/widgets/bouncing_button.dart';
 import 'package:flutter_app/presentation/components/widgets/buttons.dart';
 import 'package:flutter_app/presentation/components/widgets/styles.dart';
 import 'package:flutter_app/presentation/tab_bar/tab_bar_page.dart';
+import 'package:flutter_app/utils/constants.dart';
 import 'package:flutter_app/utils/nx_colors.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GradientedBackground(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Image(image: AssetImage('assets/images/logo.png')),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    'НАЙМИКС',
-                    style: primaryText18,
+    return SafeArea(
+      child: GradientedBackground(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image(image: AssetImage('assets/images/logo.png')),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage('assets/icons/emoji_monocle.png'),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'НАЙМИКС',
+                      style: primaryText18,
                     ),
-                    Text(
-                      'Вход',
-                      style: primaryText34,
-                    )
-                  ],
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: NXColors.inputFieldFillColor,
-                    border: OutlineInputBorder(),
-                    hintText: 'Введите логин',
-                    hintStyle: secondaryText16,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                  child: TextFormField(
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60.0, bottom: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/icons/emoji_monocle.png'),
+                        ),
+                        Text(
+                          'Вход',
+                          style: primaryText34,
+                        )
+                      ],
+                    ),
+                  ),
+                  TextFormField(
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: NXColors.inputFieldFillColor,
-                      border: OutlineInputBorder(),
-                      hintText: 'Введите пароль',
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(Constants.widgetBorderRadius)),
+                      hintText: 'Введите логин',
                       hintStyle: secondaryText16,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
-                  child: FilledActionButton(
-                      text: 'Забыли пароль', onPressed: () {}),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: GradientedActionButton(
-                      text: 'Войти',
-                      onPressed: () => Navigator.of(context).push(
-                          MaterialPageRoute(
-                              fullscreenDialog: true,
-                              builder: (context) => TabBarPage()))),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: NXColors.inputFieldFillColor,
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(Constants.widgetBorderRadius)),
+                        hintText: 'Введите пароль',
+                        hintStyle: secondaryText16,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32.0),
+                    child: BouncingButton(
+                      child: FilledActionButton(
+                        text: 'Забыли пароль',
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: BouncingButton(
+                      child: GradientedActionButton(
+                          text: 'Войти',
+                          onPressed: () => Navigator.of(context).push(MaterialPageRoute(fullscreenDialog: true, builder: (context) => TabBarPage()))),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
