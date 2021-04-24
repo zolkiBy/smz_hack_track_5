828bdffba10fae8be364fdb94c50cd2fb9fb82ed
Vitali Yashchuk <dev.yaschchuk@gmail.com>
2021-04-24 13:36:37 +0300

    change text in input fields on login screen

--- lib/presentation/components/widgets/styles.dart
+++ lib/presentation/components/widgets/styles.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/utils/nx_colors.dart';
 
+const primaryText16 = TextStyle(color: NXColors.textPrimary, fontWeight: FontWeight.w700, fontSize: 16.0);
 const primaryText18 = TextStyle(color: NXColors.textPrimary, fontWeight: FontWeight.w700, fontSize: 18.0);
 const primaryText34 = TextStyle(color: NXColors.textPrimary, fontWeight: FontWeight.w700, fontSize: 34.0);
 
